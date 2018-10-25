f6800472415d70aa30ba5994bc0ebfa389e55f0c
grantpant <grantpant@gmail.com>
2018-10-25 18:34:23 -0500

    code polishing

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const request = require('request');
-const yelp = require('yelp-fusion');
+const request = require("request");
+const yelp = require("yelp-fusion");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-app.get('/', function(req, res){
-    res.render('home.ejs');
+app.get("/", function(req, res) {
+  res.render("home.ejs");
 });
 
-app.get('/searchresults', function(req, res){
-    var city = req.query.myCity;
-    console.log(city);
+app.get("/searchresults", function(req, res) {
+  var city = req.query.myCity;
+  console.log(city);
 
-    // Yelp-fusion code sampled from "https://github.com/Yelp/yelp-fusion/blob/master/fusion/node/sample.js"
-    var searchRequest = {
-        location: city,
-        limit: 10
-    };
+  // Yelp-fusion code sampled from "https://github.com/Yelp/yelp-fusion/blob/master/fusion/node/sample.js"
+  var searchRequest = {
+    location: city,
+    limit: 10
+  };
 
-    'use strict';
+  ("use strict");
 
-    const apiKey = 'NVnRqA_Yk1SeKQVxxbBnE37tM9N7sPxAs7o2JwglhPty2RvxgFzoqGP75O_7dyKhw8PWY1iLuDVPe7VfK6OhkHacxRQYw0Q5i0oN1BqQNVDQUfO2lEPf4AmU7l0kW3Yx';
+  const apiKey = "NVnRqA_Yk1SeKQVxxbBnE37tM9N7sPxAs7o2JwglhPty2RvxgFzoqGP75O_7dyKhw8PWY1iLuDVPe7VfK6OhkHacxRQYw0Q5i0oN1BqQNVDQUfO2lEPf4AmU7l0kW3Yx";
 
-    const client = yelp.client(apiKey);
-    client.search(searchRequest).then(response => {
-        var prettyStuff = JSON.stringify(response.jsonBody.businesses, null, 4);
+  const client = yelp.client(apiKey);
+  client.search(searchRequest)
+    .then(response => {
+      var prettyStuff = JSON.stringify(response.jsonBody.businesses, null, 4);
 
-        var stuff = JSON.parse(prettyStuff);
-        
-        res.render('searchresults.ejs', {
-            stuff: stuff
-        });
-    }).catch(e => {
-        console.log(e);
-    });
+      var stuff = JSON.parse(prettyStuff);
+
+      res.render("searchresults.ejs", {
+        stuff: stuff
+      });
+    })
+    .catch(e => console.log(e));
 });
 
-app.listen(3000, function(){
-    console.log('Listening on port 3000...');
+app.listen(3000, function() {
+  console.log("Listening on port 3000...");
 });
